1b0e5081930ba534cbc6b344a20309b179cbda4c
Hai Vu <haivu2004@gmail.com>
2025-03-11 11:47:19 -0700

    Add test for bulllet format with custom width

--- src/test/test_bullet.py
+++ src/test/test_bullet.py
@@ -32,18 +32,23 @@ def test_indentation(text, expected):
 
 @pytest.fixture(
     params=[
-        pytest.param(("asterisk_input.md", "asterisk_output.md"), id="asterisk"),
-        pytest.param(("dash_input.md", "dash_output.md"), id="dash"),
-        pytest.param(("indent_input.md", "indent_output.md"), id="indent"),
-        pytest.param(("mixed_input.md", "mixed_output.md"), id="mixed"),
+        pytest.param(("asterisk_input.md", "asterisk_output.md", {}), id="asterisk"),
+        pytest.param(("dash_input.md", "dash_output.md", {}), id="dash"),
+        pytest.param(("indent_input.md", "indent_output.md", {}), id="indent"),
+        pytest.param(("mixed_input.md", "mixed_output.md", {}), id="mixed"),
+        pytest.param(
+            ("width_input.md", "width_output.md", {"width": 65}), id="custom_width"
+        ),
     ]
 )
 def format_data(request, data_dir):
     return types.SimpleNamespace(
         input_text=(data_dir / request.param[0]).read_text("utf-8"),
         expected=(data_dir / request.param[1]).read_text("utf-8"),
+        kwargs=request.param[2],
     )
 
 
 def test_format_bullet(format_data):
-    assert bullet.format(format_data.input_text) == format_data.expected
+    actual = bullet.format(format_data.input_text, **format_data.kwargs)
+    assert actual == format_data.expected
